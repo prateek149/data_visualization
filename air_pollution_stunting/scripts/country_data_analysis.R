ecb1289d1e9042c7d8268d3f12c9dfeea1c90d12
Prateek <prateek149@gmail.com>
2017-01-02 14:45:36 +0530

    air pollution op-ed

---
+++ air_pollution_stunting/scripts/country_data_analysis.R
@@ -0,0 +1,42 @@
+library("plyr")
+library("ggplot2")
+library("ggthemes")
+setwd("/Users/prateekmittal/Desktop/data_visualization/air_pollution_stunting")
+
+a <- read.csv("data/stunting_data.csv", stringsAsFactors = F)
+
+filter <- c("INDIA","PAKISTAN","BANGLADESH","INDONESIA","SRI LANKA","AFGHANISTAN")
+
+b <- a[which(a$country %in% filter),]
+
+### Creating a time variable which will take values 1 or 2. 
+### 1 for the earliest records in 1990s & 2 for the latest available record##
+b$time <- 0
+
+# identifying the latest record for each country ##
+for (i in filter) {
+  t1 <- min(b[which(b$country==i & b$year>1990),]$year)
+  t2 <- max(b[which(b$country==i),]$year)
+  b[which(b$country==i & b$year==t1),]$time <- 1
+  b[which(b$country==i & b$year==t2),]$time <- 2
+}
+
+## dealing with missing values in Indonesia ###
+b[which(b$country=="INDONESIA" & b$time==1),]$time <- 0
+b[which(b$country=="INDONESIA" & b$year==1995),]$time <- 1
+
+c <- b[which(b$time>0),]
+
+c <- c[,c("country","stunting","year","time")]
+
+c$xlabel = "1990s"
+c[which(c$time==2),]$xlabel = "Latest"
+
+#### Exploratory Plot ################
+test1 <- ggplot(c, aes(time, stunting)) + geom_line(aes(color=country)) +
+  theme_minimal() +
+  scale_colour_brewer(palette="Set1") +
+  scale_y_continuous(breaks=seq(0,100,20), limits=c(0,100)) +
+  scale_x_continuous(breaks=seq(1,2),labels=c("1990s","Latest"))
+
+ggsave("plots/stunting_countries.png", plot=test1, width=10, height = 10)
